b8dfe15dd921be0e5898c7bbd6c1b37ba825f69a
Alan Maciel <alan.maciel.salcedo@gmail.com>
2024-07-14 11:11:59 -0600

    Turbo Drive

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,3 +3,13 @@ import "@hotwired/turbo-rails"
 import "controllers"
 import "trix"
 import "@rails/actiontext"
+
+// To desactivate TurboDrive session:
+// Turbo.session.drive = false
+// To desactivate TurboDrive in a link:
+// <%= link_to "No Turbo", some_path, data: { turbo: false } %>
+// To desactivate TurboDrive in a form:
+// <%= form_with(url: some_path, data: { turbo: false } do |f| )
+// ...
+// <% end %>
+Turbo.setProgressBarDelay(10)
